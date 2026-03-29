4f284a54a1ab91a39c3266aeeba9bf1eba6b7052
Hasan-TechNinja <hasantechninja@gmail.com>
2026-03-29 10:19:34 +0600

    Fix Python path for Django

--- api/index.py
+++ api/index.py
@@ -1,6 +1,15 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+# Get current directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add project root to Python path
+sys.path.insert(0, BASE_DIR)
+
+# Set correct settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")  # 👈 change if needed
+
+from django.core.wsgi import get_wsgi_application
 
 app = get_wsgi_application()
